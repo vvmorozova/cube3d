76c9e16f70517cf81e1ba738b650003de841b120
Butcher Sarai <bsarai@at-n2.msk.21-school.ru>
2022-10-09 15:51:01 +0300

    refactor t_data: rename t_data to t_img

--- graphics/src/graphics_utils.c
+++ graphics/src/graphics_utils.c
@@ -1,14 +1,14 @@
 #include "graphics.h"
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int *) dst = color;
 }
 
-void	verLine(t_data *img, int x, int drawStart, int drawEnd, int color)
+void	verLine(t_img *img, int x, int drawStart, int drawEnd, int color)
 {
 	while (drawStart < drawEnd)
 	{
--- graphics/src/main.c
+++ graphics/src/main.c
@@ -67,7 +67,7 @@ int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
+	t_img	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
