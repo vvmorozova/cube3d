77132c204a3b770da4c74a103511c14ba0a15090
Butcher Sarai <bsarai@at-n2.msk.21-school.ru>
2022-10-19 18:02:59 +0300

    norm main

--- src/main.c
+++ src/main.c
@@ -7,28 +7,55 @@
 #include "graphics.h"
 #include "cube3d.h"
 
-// char world_map[mapWidth][mapHeight];
+static void	choose_dir_e_w(t_data *data, char dir);
+static void	choose_dir(t_data	*data, char dir);
+static void	all_data_init(t_all_data *a_data, t_mlx	*mlx_data,
+				t_parsed_map	*g_map, t_data *data);
 
-void	choose_dir_e_w(t_data *data, char dir)
+int	main(int argc, char **argv)
 {
-	if (dir == 'E' || dir == 'W')
-	{
-		data->plane_y = 0;
-		data->dir_x = 0;
-		if (dir == 'E')
-		{
-			data->dir_y = 1;
-			data->plane_x = 0.66;
-		}
-		if (dir == 'W')
-		{
-			data->dir_y = -1;
-			data->plane_x = -0.66;
-		}
-	}
+	t_parsed_map	g_map;
+	t_mlx			mlx_data;
+	t_data			data;
+	t_all_data		all_data;
+
+	parse(argc, argv, &g_map);
+	all_data_init(&all_data, &mlx_data, &g_map, &data);
+	redraw(&all_data);
+	mlx_hook(mlx_data.mlx_win, 17, 1L << 17, close_window, &mlx_data);
+	mlx_key_hook(mlx_data.mlx_win, key_pressed, &all_data);
+	mlx_loop(mlx_data.mlx);
 }
 
-void	choose_dir(t_data	*data, char dir)
+static void	all_data_init(t_all_data *a_data, t_mlx	*mlx_data,
+		t_parsed_map	*g_map, t_data *data)
+{
+	char	**txts_path;
+
+	txts_path = g_map->txts_path;
+	mlx_data->mlx = mlx_init();
+	mlx_data->mlx_win = mlx_new_window(mlx_data->mlx,
+			screenWidth, screenHeight, "Cub");
+	mlx_data->img.img = mlx_new_image(mlx_data->mlx, screenWidth, screenHeight);
+	mlx_data->img.addr = mlx_get_data_addr(mlx_data->img.img,
+			&mlx_data->img.bits_per_pixel, &mlx_data->img.line_length,
+			&mlx_data->img.endian);
+	a_data->mlx_data = mlx_data;
+	data->pos_x = g_map->pos.x;
+	data->pos_y = g_map->pos.y;
+	choose_dir(data, g_map->direction);
+	a_data->data = data;
+	a_data->map_data = g_map;
+	if (init_txtrs(mlx_data->mlx, txts_path, a_data->txtrs))
+	{
+		free_txt_paths(txts_path);
+		mlx_destroy_window(mlx_data->mlx, mlx_data->mlx_win);
+		exit(1);
+	}
+	free_txt_paths(txts_path);
+}
+
+static void	choose_dir(t_data	*data, char dir)
 {
 	if (dir == 'S' || dir == 'N')
 	{
@@ -48,55 +75,21 @@ void	choose_dir(t_data	*data, char dir)
 	choose_dir_e_w(data, dir);
 }
 
-// void	print_paths(char *txts_path[])
-// {
-// 	for (int i = 0; i < 4; i++)
-// 		printf("path[%d]: %s\n", i, txts_path[i]);
-// }
-
-
-
-int	main(int argc, char **argv)
+static void	choose_dir_e_w(t_data *data, char dir)
 {
-	t_parsed_map	g_map;
-	parse(argc, argv, &g_map);
-
-	char	**txts_path = g_map.txts_path;
-	t_mlx	mlx_data;
-
-	mlx_data.mlx = mlx_init();
-	mlx_data.mlx_win = mlx_new_window(mlx_data.mlx, screenWidth, screenHeight, "Cub");
-	mlx_data.img.img = mlx_new_image(mlx_data.mlx, screenWidth, screenHeight);
-	mlx_data.img.addr = mlx_get_data_addr(mlx_data.img.img, &mlx_data.img.bits_per_pixel, &mlx_data.img.line_length,
-								&mlx_data.img.endian);
-
-	t_data	data;
-	data.pos_x = g_map.pos.x,	data.pos_y = g_map.pos.y;
-	choose_dir(&(data), g_map.direction);
-	// data.plane_x = 0.66, data.plane_y = 0;
-
-	t_all_data	all_data;
-	all_data.mlx_data = &mlx_data;
-	all_data.data = &data;
-	all_data.map_data = &g_map;
-
-	if (init_txtrs(mlx_data.mlx, txts_path, all_data.txtrs))
+	if (dir == 'E' || dir == 'W')
 	{
-		// clean paths
-		free_txt_paths(txts_path);
-		// destroy main image
-		mlx_destroy_window(mlx_data.mlx, mlx_data.mlx_win);
-		exit(1);
+		data->plane_y = 0;
+		data->dir_x = 0;
+		if (dir == 'E')
+		{
+			data->dir_y = 1;
+			data->plane_x = 0.66;
+		}
+		if (dir == 'W')
+		{
+			data->dir_y = -1;
+			data->plane_x = -0.66;
+		}
 	}
-	free_txt_paths(txts_path);
-	
-	// mlx_put_image_to_window(mlx_data.mlx, mlx_data.mlx_win, txtrs[3].img.img, 0, 0);
-
-
-	redraw(&all_data);
-	mlx_hook(mlx_data.mlx_win, 17, 1L << 17, close_window, &mlx_data);
-	mlx_key_hook(mlx_data.mlx_win, key_pressed, &all_data);
-
-	// mlx_destroy_image
-	mlx_loop(mlx_data.mlx);
 }
